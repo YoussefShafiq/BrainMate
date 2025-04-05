7be0465b31ccbeb300f3ba796f0ae1aacf1178dd
Youssef Shafek <youssefmsa616@gmail.com>
2025-04-05 19:17:59 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import brainmateLogo from '/brainmate.png'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="text-red-500 text-3xl">
+        hello
+      </div>
+    </>
+  )
+}
+
+export default App
